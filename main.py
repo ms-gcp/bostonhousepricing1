9d465c525d7cb2521e24d84b8d458cb8f539ddf4
ms-gcp <ms.gcp123@gmail.com>
2023-01-23 23:43:34 +0530

    ...

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import pickle
 
@@ -34,4 +35,4 @@ def predict():
 
 
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
